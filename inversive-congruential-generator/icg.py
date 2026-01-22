5fc121fd5b60316dc87d503c6e5c4958be2eede8
Abdullah Bagyapan <229599340+abdullahbagyapan@users.noreply.github.com>
2026-01-22 19:09:19 +0300

    feat: add inversive congruential generator

---
+++ inversive-congruential-generator/icg.py
@@ -0,0 +1,33 @@
+from sympy import isprime, mod_inverse
+
+class InversiveCongruentialGenerator:
+
+    def __init__(self, seed = 1, multiplier = 16807, increment = 1, modulus = 2**31 - 1):
+
+        if not isprime(modulus):
+            raise ValueError("modulus is not prime number")
+
+        self.seed = seed
+        self.multiplier = multiplier
+        self.increment = increment
+        self.modulus = modulus
+
+    def srand(self):
+        
+        if (self.seed == 0):
+            self.seed = self.increment
+        else:
+            inv = mod_inverse(self.seed, self.modulus)
+            self.seed = ((self.multiplier * inv) + self.increment) % self.modulus
+
+        return self.seed
+        
+
+
+if __name__ == "__main__":
+
+    icg = InversiveCongruentialGenerator()
+
+    for i in range(10):
+        rand = icg.srand()
+        print(rand)
